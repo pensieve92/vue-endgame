88f2212ce620d1e417b226b2b83b9a78c1fff281
byeongjeon jeon <pensieve92@gmail.com>
2022-01-09 19:41:02 +0900

    초기 진입 페이지 설정

--- vue-til/src/routes/index.js
+++ vue-til/src/routes/index.js
@@ -6,6 +6,10 @@ Vue.use(VueRouter);
 export default new VueRouter({
   routes: [
     {
+      path: '/',
+      redirect: '/login',
+    },
+    {
       path: '/login',
       component: () => import('@/views/LoginPage.vue'), // 코드스플리팅
     },
